e4f6099875b5af46b9217f08b5e7e5243e7e4ae8
viniizn <jovini1303@gmail.com>
2026-02-15 23:06:38 -0300

    Iniciando

---
+++ src/test/java/to_do/example/to_do/ToDoApplicationTests.java
@@ -0,0 +1,13 @@
+package to_do.example.to_do;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
