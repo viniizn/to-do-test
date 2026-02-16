56193d915583a9e9265903b898ce0537c538ab63
viniizn <jovini1303@gmail.com>
2026-02-15 23:14:03 -0300

    entities

---
+++ src/main/java/to_do/example/to_do/entities/ToDo.java
@@ -0,0 +1,55 @@
+package to_do.example.to_do.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "todos")
+public class ToDo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private boolean realizado;
+    private int prioridade;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isRealizado() {
+        return realizado;
+    }
+
+    public void setRealizado(boolean realizado) {
+        this.realizado = realizado;
+    }
+
+    public int getPrioridade() {
+        return prioridade;
+    }
+
+    public void setPrioridade(int prioridade) {
+        this.prioridade = prioridade;
+    }
+}
