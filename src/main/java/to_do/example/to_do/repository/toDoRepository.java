ac95d18e0cf68886aab2f9e8e4efde98ffc93696
viniizn <jovini1303@gmail.com>
2026-02-15 23:16:27 -0300

    Definindo dominio

---
+++ src/main/java/to_do/example/to_do/repository/toDoRepository.java
@@ -0,0 +1,7 @@
+package to_do.example.to_do.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import to_do.example.to_do.entities.ToDo;
+
+public interface toDoRepository extends JpaRepository<ToDo, Long> {
+}
